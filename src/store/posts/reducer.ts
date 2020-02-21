7009283d08cc720290c9e8f0551ede4dac0e3b41
josemak25 <amakirij@gmail.com>
2020-02-21 08:23:03 +0100

    add post context reducer

---
+++ src/store/posts/reducer.ts
@@ -0,0 +1,49 @@
+import { POST_TYPES, PostInitialState, PostAction } from './types';
+
+export const postInitialState: PostInitialState = {
+  isLoading: false,
+  error: null,
+  posts: []
+};
+
+export default function postReducer(
+  state = postInitialState,
+  action: PostAction
+): PostInitialState {
+  switch (action.type) {
+    case POST_TYPES.LOAD_POST_STARTED: {
+      return { ...state, isLoading: true };
+    }
+
+    case POST_TYPES.LOAD_POST_SUCCESS: {
+      return {
+        ...state,
+        isLoading: false,
+        error: null,
+        posts: [...state.posts, action.payload]
+      };
+    }
+
+    case POST_TYPES.LIKE_POST: {
+      const { payload: postId } = action;
+      // send id to backend
+    }
+
+    case POST_TYPES.LIKE_COMMENT: {
+      const { payload: commentId } = action;
+      // send id to backend
+    }
+
+    case POST_TYPES.COMMENT_ON_POST: {
+      const { postId, comment } = action.payload;
+      // send id to backend
+    }
+
+    case POST_TYPES.LOAD_POST_ERROR: {
+      return { ...state, isLoading: false, error: action.payload };
+    }
+
+    default:
+      return state;
+  }
+}
