e7fdc6e46b340534f09fa62a557eb19ec2c86027
josemak25 <amakirij@gmail.com>
2020-02-21 09:19:51 +0100

    add app component and connected app loading component to help catch all expected assets on app load

--- App.tsx
+++ App.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import AppLoading from './src/components/AppLoading';
+import AppRouter from './src';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+  const [isAppReady, setIsAppReady] = useState(false);
+
+  return isAppReady ? (
+    <AppRouter />
+  ) : (
+    <AppLoading setIsAppReady={setIsAppReady} />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
