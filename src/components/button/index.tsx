bbc9f14c64d21bc2f4dac420d8ab3a9bad57adf5
josemak25 <amakirij@gmail.com>
2020-02-21 08:42:41 +0100

    add button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import boxShadow from '../../utils/boxShadows';
+
+import { Container, ButtonText } from './styles';
+
+type ButtonProps = {
+  buttonStyle?: {};
+  textStyle?: {};
+  testID?: string;
+  title: string;
+  onPress?(): void;
+};
+
+export default function Button(props: ButtonProps) {
+  const { title, buttonStyle, textStyle } = props;
+
+  return (
+    <Container style={[boxShadow({ elevation: 2 }), buttonStyle]} {...props}>
+      <ButtonText style={textStyle}>{title}</ButtonText>
+    </Container>
+  );
+}
