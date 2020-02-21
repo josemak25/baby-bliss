1a88f7b64f436e39a310992301ad4c3e12480a2c
josemak25 <amakirij@gmail.com>
2020-02-21 08:28:23 +0100

    add configuration for global store

---
+++ src/store/index.tsx
@@ -0,0 +1,22 @@
+import React, { createContext, useReducer, FunctionComponent } from 'react';
+import useCombinedReducers from '../utils/useCombinedReducers';
+
+import postsReducer, { postInitialState } from './posts/reducer';
+import userReducer, { userInitialState } from './user/reducer';
+
+const StoreContext = createContext(null);
+
+const StoreProvider: FunctionComponent = ({ children }) => {
+  const [state, dispatch] = useCombinedReducers({
+    user: useReducer(userReducer, userInitialState),
+    posts: useReducer(postsReducer, postInitialState)
+  });
+
+  return (
+    <StoreContext.Provider value={[state, dispatch]}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
+
+export { StoreProvider, StoreContext };
