d6cf80b6f0ebb11d0fe3d540d93220746e34d0f0
josemak25 <amakirij@gmail.com>
2020-02-22 16:17:23 +0100

    add change for font family name

--- src/theme/types.ts
+++ src/theme/types.ts
@@ -17,11 +17,12 @@ enum FONTS {
   SMALL_SIZE = 10,
   MEDIUM_SIZE = 12,
   LARGE_SIZE = 15,
-  FAMILY_REGULAR = 'montserrat-regular',
-  FAMILY_MEDIUM = 'montserrat-medium',
-  FAMILY_BOLD = 'montserrat-bold',
-  FAMILY_SEMI_BOLD = 'montserrat-semi-bold',
-  FAMILY_IBM = 'IBM-regular',
+  MONTSERRAT_REGULAR = 'montserrat-regular',
+  MONTSERRAT_MEDIUM = 'montserrat-medium',
+  MONTSERRAT_BOLD = 'montserrat-bold',
+  MONTSERRAT_SEMI_BOLD = 'montserrat-semi-bold',
+  IBM_SANS_REGULAR = 'IBM-regular',
+  PACIFICO_REGULAR = 'pacifico-regular',
   FONT_WEIGHT_LIGHT = 200,
   FONT_WEIGHT_MEDIUM = 600,
   FONT_WEIGHT_HEAVY = 800
@@ -34,7 +35,11 @@ declare module 'styled-components' {
       BD_DARK_COLOR: string;
       BG_LIGHT_COLOR: string;
       FONT_DARK_COLOR: string;
-      FONT_MILK_COLOR: string;
+      FONT_LIGHT_COLOR: string;
+      INACTIVE_ICON_COLOR: string;
+      LIKE_POST_COLOR: string;
+      POST_TIP_COLOR: string;
+      FLOATING_MESSAGE_COLOR: string;
     };
 
     // All Global App Font Sizes
@@ -42,11 +47,12 @@ declare module 'styled-components' {
       SMALL_SIZE: number;
       MEDIUM_SIZE: number;
       LARGE_SIZE: number;
-      FAMILY_REGULAR: string;
-      FAMILY_MEDIUM: string;
-      FAMILY_BOLD: string;
-      FAMILY_SEMI_BOLD: string;
-      FAMILY_IBM: string;
+      MONTSERRAT_REGULAR: string;
+      MONTSERRAT_MEDIUM: string;
+      MONTSERRAT_BOLD: string;
+      MONTSERRAT_SEMI_BOLD: string;
+      IBM_SANS_REGULAR: string;
+      PACIFICO_REGULAR: string;
       FONT_WEIGHT_LIGHT: number;
       FONT_WEIGHT_MEDIUM: number;
       FONT_WEIGHT_HEAVY: number;
