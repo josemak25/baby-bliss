0ac44154b6b33063bedb05aed674e386530b838b
josemak25 <amakirij@gmail.com>
2020-02-24 11:35:47 +0100

    add splash component for initial app start

--- src/screens/splash/index.tsx
+++ src/screens/splash/index.tsx
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { ThemeContext } from 'styled-components';
 
-import Button from '../../components/button';
-import { Container, Welcome } from './styles';
+import { Container, ImageContainer, Image } from './styles';
 
-export default function Splash() {
+export default function SplashScreen({ navigation }) {
+  const { colors } = useContext(ThemeContext);
+
+  const [splash, setSplash] = useState({
+    appLogoLoaded: true,
+    pregnancyLogoLoaded: false
+  });
+
+  useEffect(() => {
+    setTimeout(
+      () =>
+        setSplash({
+          ...splash,
+          appLogoLoaded: false,
+          pregnancyLogoLoaded: true
+        }),
+      4000
+    );
+    setTimeout(() => navigation.replace('HomeScreen'), 8000);
+  }, []);
+
   return (
-    <Container>
-      <Welcome>Splash Screen</Welcome>
-      <Button title="Splash screen button" />
+    <Container
+      start={{ x: 0, y: 1 }}
+      end={{ x: 0, y: 0 }}
+      colors={[colors.GRADIENT_COLOR_FROM, colors.GRADIENT_COLOR_TO]}
+    >
+      {splash.appLogoLoaded && (
+        <ImageContainer>
+          <Image
+            style={{ resizeMode: 'contain', width: '100%', height: '100%' }}
+            source={require('../../../assets/images/splash.png')}
+            fadeDuration={0}
+          />
+        </ImageContainer>
+      )}
+
+      {splash.pregnancyLogoLoaded && (
+        <Image
+          style={{ resizeMode: 'stretch' }}
+          source={require('../../../assets/images/splash_pregnancy.png')}
+          fadeDuration={0}
+        />
+      )}
+
+      {splash.pregnancyLogoLoaded ? (
+        <ActivityIndicator
+          size="large"
+          color={colors.POST_TIP_COLOR}
+          style={{ position: 'absolute', bottom: 100 }}
+        />
+      ) : null}
     </Container>
   );
 }
+
+SplashScreen.navigationOptions = {
+  header: () => null
+};
