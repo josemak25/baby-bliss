e2a5f3423bf9303c08392a060ca3d1247593cd4b
Izuking <izuking@okra.ng>
2020-02-24 22:50:44 +0100

    rf-add-some-new-colors-for global-use

--- src/theme/types.ts
+++ src/theme/types.ts
@@ -2,14 +2,16 @@ import 'styled-components';
 
 // All app colors
 enum COLORS {
-  BD_DARK_COLOR = '#FFFFFF',
+  BD_DARK_COLOR = '#F4F8FB',
   BG_LIGHT_COLOR = '#FFFFFF',
   FONT_DARK_COLOR = '#0D0E10',
   FONT_LIGHT_COLOR = '#2E2D2D',
   INACTIVE_ICON_COLOR = '#D7D8DA',
   LIKE_POST_COLOR = '#F42850',
   POST_TIP_COLOR = '#50AE7C',
-  FLOATING_MESSAGE_COLOR = '#3CB6AB'
+  FLOATING_MESSAGE_COLOR = '#3CB6AB',
+  TEXT_INPUT_BORDER_COLOR = '#97989A',
+  IDLE_INPUT_COLOR = '#EEF2F5'
 }
 
 // All app font sizes
@@ -40,6 +42,8 @@ declare module 'styled-components' {
       LIKE_POST_COLOR: string;
       POST_TIP_COLOR: string;
       FLOATING_MESSAGE_COLOR: string;
+      TEXT_INPUT_BORDER_COLOR: string;
+      IDLE_INPUT_COLOR: string;
     };
 
     // All Global App Font Sizes
