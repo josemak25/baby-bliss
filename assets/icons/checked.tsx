f857ed9af37b14e1ac356d5925f1782f285328c4
Izuking <izuking@okra.ng>
2020-02-25 12:15:33 +0100

    ch-Add-icons

--- assets/icons/checked.tsx
+++ assets/icons/checked.tsx
@@ -2,16 +2,20 @@ import * as React from 'react';
 import Svg, { Path } from 'react-native-svg';
 import { IconProps } from '../../src/constants';
 
-export default function Checked(props: IconProps) {
+export default function CheckedIcon(props: IconProps) {
   return (
     <Svg
-      width="40%"
-      height="40%"
-      fillRule="evenodd"
-      clipRule="evenodd"
+      width="100%"
+      height="100%"
+      viewBox="0 0 512 512"
       {...props}
     >
-      <Path d="M19 22H0V3h19v2h-1V4H1v17h17v-9.502h1V22zm5-19.315L9.034 18.557 3.476 12l.762-.648 4.833 5.707L23.272 2l.728.685z" />
+      <Path
+        d="M256 0C114.844 0 0 114.844 0 256s114.844 256 256 256 256-114.844 256-256S397.156 0 256 0zm146.207 182.625L217.75 367.083a21.263 21.263 0 01-15.083 6.25 21.261 21.261 0 01-15.083-6.25L88.46 267.958c-4.167-4.165-4.167-10.919 0-15.085l15.081-15.082c4.167-4.165 10.919-4.165 15.086 0l84.04 84.042L372.04 152.458c4.167-4.165 10.919-4.165 15.086 0l15.081 15.082c4.167 4.166 4.167 10.92 0 15.085z"
+        data-original="#000000"
+        data-old_color="#000000"
+        fill="#00AA39"
+      />
     </Svg>
   );
 }
