9c9c0c02fd6b39c2b56b2a28de3db733f19085d5
Izuking <izuking@okra.ng>
2020-02-26 10:46:03 +0100

    ft-add-community-screen-skeleton

---
+++ src/screens/community/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Button from '../../components/button';
+import { Container, Welcome } from './styles';
+import { NavigationInterface } from '../../constants';
+
+interface CommunityScreenProp extends NavigationInterface {
+  testID?: string;
+}
+
+export default function CommunityScreen(props: CommunityScreenProp) {
+  return (
+    <Container>
+      <Button title="Community Screen button" />
+      <Welcome>Community Screen</Welcome>
+    </Container>
+  );
+}
+
+CommunityScreen.navigationOptions = ({ navigationOptions }) => {
+  return {
+    ...navigationOptions,
+    headerTitle: () => null,
+    headerLeft: () => null,
+    headerStyle: { backgroundColor: '#F4F8FB' }
+  };
+};
