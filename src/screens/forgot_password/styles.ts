a447d7cc22d2082ebc60c4b982cc936b0fde91c3
josemak25 <amakirij@gmail.com>
2020-02-26 10:55:49 +0100

    add forgot password screen styles

--- src/screens/forgot_password/styles.ts
+++ src/screens/forgot_password/styles.ts
@@ -1,5 +1,13 @@
 import styled from 'styled-components/native';
 
+export const SafeAreaView = styled.SafeAreaView`
+  flex: 1;
+`;
+
+export const KeyboardAvoidingView = styled.KeyboardAvoidingView`
+  flex: 1;
+`;
+
 export const Container = styled.View`
   flex: 1;
   align-items: center;
@@ -14,32 +22,38 @@ export const HeaderTitle = styled.Text`
 `;
 
 export const ImageContainer = styled.View`
+  flex: 1;
   width: 100%;
-  height: 250px;
   justify-content: center;
   align-items: center;
   background-color: ${({ theme }) => theme.colors.BD_DARK_COLOR};
 `;
 
 export const TitleContainer = styled.View`
+  flex: 0.5;
   width: 100%;
-  height: 11%;
-  justify-content: space-between;
   align-items: center;
-  padding: 0 40px;
 `;
 
 export const Title = styled.Text`
+  width: 100%;
   font-size: ${({ theme }) => theme.fonts.LARGE_SIZE + 20}px;
   font-family: ${({ theme }) => theme.fonts.MONTSERRAT_MEDIUM};
   color: ${({ theme }) => theme.colors.FONT_DARK_COLOR};
   text-transform: capitalize;
+  text-align: center;
 `;
 
 export const TitleContent = styled.Text`
+  width: 80%;
   font-size: ${({ theme }) => theme.fonts.LARGE_SIZE}px;
   font-family: ${({ theme }) => theme.fonts.MONTSERRAT_MEDIUM};
   color: ${({ theme }) => theme.colors.FONT_LIGHT_COLOR};
   text-align: center;
-  padding: 0 15px;
+  padding: 5px 10px;
+`;
+
+export const FormField = styled.View`
+  flex: 1;
+  padding: 0 20px;
 `;
