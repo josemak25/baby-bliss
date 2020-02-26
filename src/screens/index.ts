d7e3d0c146c6586778484e1161734b449541a668
Izuking <izuking@okra.ng>
2020-02-26 14:56:56 +0100

    ft-add-bottom-tab-nav

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,5 +2,14 @@ import HomeScreen from './home';
 import SplashScreen from './splash';
 import GetStartedScreen from './get_started';
 import SignUpScreen from './signup';
+import CommunityScreen from './community';
+import ProfileScreen from './profile';
 
-export default { SignUpScreen, HomeScreen, SplashScreen, GetStartedScreen };
+export default {
+  SignUpScreen,
+  HomeScreen,
+  SplashScreen,
+  GetStartedScreen,
+  CommunityScreen,
+  ProfileScreen
+};
