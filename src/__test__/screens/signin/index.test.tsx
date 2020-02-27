99ab46131fa107417bb6cf3dc0ee2bbf76d145df
josemak25 <amakirij@gmail.com>
2020-02-27 10:00:36 +0100

    add signin screen test

---
+++ src/__test__/screens/signin/index.test.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react-native';
+import Theme from '../../../theme';
+import { createNavigationTestProps } from '../../../constants';
+import SignInScreen from '../../../screens/signin';
+
+const mountComponent = () => {
+  const props: any = createNavigationTestProps();
+  const renderedProps = render(
+    <Theme>
+      <SignInScreen {...props} />
+    </Theme>
+  );
+  return { props, ...renderedProps };
+};
+
+describe(' TEST SIGN_IN SCREEN (<SignInScreen/>)', () => {
+  test('It renders the App logo component', () => {
+    const { queryByTestId } = mountComponent();
+    expect(queryByTestId('appLogo')).toBeTruthy();
+  });
+
+  test('That the screen has all the necessary input fields', () => {
+    const { getByTestId } = mountComponent();
+    expect(getByTestId('email')).toBeTruthy();
+    expect(getByTestId('password')).toBeTruthy();
+  });
+
+  test('That the forgot password button was pressed', () => {
+    const { props, queryByTestId } = mountComponent();
+    const signUpButton = queryByTestId('resetAccount');
+    expect(signUpButton).toBeTruthy();
+    fireEvent.press(queryByTestId('resetAccount'));
+    expect(props.navigation.navigate).toHaveBeenCalledTimes(1);
+    expect(props.navigation.navigate).toHaveBeenCalledWith(
+      'ForgotPasswordScreen'
+    );
+  });
+
+  test('That the login button was pressed', () => {
+    const { props, queryByTestId } = mountComponent();
+    const loginButton = queryByTestId('loginButton');
+    expect(loginButton).toBeTruthy();
+    fireEvent.press(queryByTestId('loginButton'));
+    expect(props.navigation.replace).toHaveBeenCalledTimes(1);
+    expect(props.navigation.replace).toHaveBeenCalledWith('HomeScreen');
+  });
+
+  test('That the sigUp button was pressed', () => {
+    const { props, queryByTestId } = mountComponent();
+    const signUpButton = queryByTestId('createAccount');
+    expect(signUpButton).toBeTruthy();
+    fireEvent.press(queryByTestId('createAccount'));
+    expect(props.navigation.navigate).toHaveBeenCalledTimes(1);
+    expect(props.navigation.navigate).toHaveBeenCalledWith('SignUpScreen');
+  });
+});
