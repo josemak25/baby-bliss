96e3840e580dfc7a424a4665e835ea8cba8552b1
Izuking <izuking@okra.ng>
2020-02-27 10:44:48 +0100

    rf-change-bottom-navigation-tab-and-adjusted-tabs-icons-properties

--- assets/icons/user.tsx
+++ assets/icons/user.tsx
@@ -3,23 +3,23 @@ import Svg, { Path, Ellipse } from 'react-native-svg';
 import { IconProps } from '../../src/constants';
 
 export default function UserIcon(props: IconProps) {
-  const { isFilled = false } = props;
+  const { fillColor } = props;
   return (
-    <Svg width="40%" height="40%" viewBox="0 0 24 24" fill="none" {...props}>
+    <Svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" {...props}>
       <Ellipse
         cx={12}
         cy={6.5}
         rx={4}
         ry={4.5}
-        stroke="#262F56"
-        fill={isFilled ? '#262F56' : '#fff'}
+        stroke={fillColor}
+        fill={fillColor === '#262F56' ? fillColor : '#fff'}
         strokeWidth={1.8}
       />
       <Path
         d="M20 19c0 3.5-4 2.5-8 2.5s-8 1-8-2.5c0-2 3.582-4.5 8-4.5s8 2.5 8 4.5z"
-        stroke="#262F56"
+        stroke={fillColor}
         strokeWidth={1.8}
-        fill={isFilled ? '#262F56' : '#fff'}
+        fill={fillColor === '#262F56' ? fillColor : '#fff'}
       />
     </Svg>
   );
