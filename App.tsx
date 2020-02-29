6880fa9b1c001823a548b9e3b86e8fbb18842e14
josemak25 <amakirij@gmail.com>
2020-02-29 13:41:20 +0100

    add app configs to app start component page

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import AppLoading from './src/components/AppLoading';
 import AppRouter from './src';
+import './src/config';
 
 export default function App() {
   const [isAppReady, setIsAppReady] = useState(false);
