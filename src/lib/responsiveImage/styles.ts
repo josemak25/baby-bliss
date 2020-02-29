9818ae8b56806e637b2c5323710564b30281f8e6
josemak25 <amakirij@gmail.com>
2020-02-29 13:30:27 +0100

    add styles for responsive image loading component

---
+++ src/lib/responsiveImage/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Image = styled.Image`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+`;
