90b291b53d9a619b903dd518818ccb846eab560a
josemak25 <amakirij@gmail.com>
2020-02-29 13:39:27 +0100

    add post card to home component to populate post on render

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { FlatList } from 'react-native';
+import Post from '../../components/post';
+import SearchIcon from '../../../assets/icons/search';
 
 import Button from '../../components/button';
-import { Container, Welcome, SafeAreaView } from './styles';
 import { NavigationInterface } from '../../constants';
 import { useStoreContext } from '../../store';
 import postsActions from '../../store/posts/actions';
-// import { POST_ACTION_TYPES } from '../../store/posts/types';
 
 interface HomeScreenProp extends NavigationInterface {
   testID?: string;
@@ -13,13 +14,34 @@ interface HomeScreenProp extends NavigationInterface {
 
 export default function HomeScreen(props: HomeScreenProp) {
   const [store, dispatch] = useStoreContext();
-  console.log(store);
   // postsActions(POST_ACTION_TYPES.LOAD_POSTS)(dispatch);
+
+
+import {
+  Container,
+  SafeAreaView,
+  Logo,
+  LogoContainer,
+  SearchContainer
+} from './styles';
+
+
+export default function HomeScreen({ post }) {
   return (
     <SafeAreaView testID="HomeScreen">
       <Container>
-        <Button title="HomeScreen screen button" />
-        <Welcome>HomeScreen Screen</Welcome>
+        <FlatList
+          data={Array(10).fill(post)}
+          renderItem={({ item, index }) => <Post {...item} postIndex={index} />}
+          keyExtractor={(_, key) => `${key}`}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 0 }}
+          onEndReachedThreshold={2}
+          onEndReached={() => console.log('END REACHED')}
+          removeClippedSubviews={true}
+          initialNumToRender={2}
+          windowSize={7}
+        />
       </Container>
     </SafeAreaView>
   );
@@ -32,6 +54,54 @@ HomeScreen.navigationOptions = ({ navigationOptions, navigation }) => {
     ...navigationBackButton,
     ...navigationOptions,
     headerTitle: () => null,
-    headerLeft: () => null
+    headerLeft: () => (
+      <LogoContainer>
+        <Logo
+          source={require('../../../assets/images/logo.png')}
+          style={{ resizeMode: 'contain' }}
+        />
+      </LogoContainer>
+    ),
+    headerRight: () => (
+      <SearchContainer>
+        <SearchIcon />
+      </SearchContainer>
+    )
   };
+};
+
+HomeScreen.defaultProps = {
+  post: {
+    images: [],
+    contentType: 'community',
+    link: null,
+    category: {
+      _id: '5d5e640247fda3145df0d912',
+      title: 'First trimester',
+      id: '5d5e640247fda3145df0d912'
+    },
+    isFlagged: false,
+    deleted: false,
+    _id: '5de102cc7fcd6d248dbbf4ee',
+    topic: 'Is it normal for my baby to move this way at 6 months?',
+    description:
+      'As conscious traveling Paupers we must always be concerned about our dear Mother Earth. If you think about it, you travel across her face.',
+    user: {
+      avatar:
+        'https://babybliss-platform.sfo2.digitaloceanspaces.com/avatar/avatar-5fcae2-H1pRhXVAkUuJ0NunOgXAhKDdl-1567081512239.png',
+      _id: '5d5fcae2848a873ccd4f1c58',
+      name: 'BabyBliss Moderator',
+      username: 'moderator',
+      id: '5d5fcae2848a873ccd4f1c58'
+    },
+    createdAt: '2019-11-29T11:36:44.699Z',
+    updatedAt: '2019-11-29T11:36:48.138Z',
+    __v: 1,
+    id: '5de102cc7fcd6d248dbbf4ee',
+    noOfComments: 0,
+    isLiked: false,
+    noOfLikes: 900,
+    noOfViews: 130,
+    userId: '5d5fcae2848a873ccd4f1c58'
+  }
 };
