de00fc8dea0340025237665b73d9f9c1d9f70d9e
josemak25 <amakirij@gmail.com>
2020-02-29 13:34:14 +0100

    rename checkbox library

--- src/screens/signin/index.tsx
+++ src/screens/signin/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import BouncyCheckbox from '../../lib/BouncyCheckbox';
+import BouncyCheckbox from '../../lib/bouncyCheckbox';
 import { useThemeContext } from '../../theme';
 import { NavigationInterface } from '../../constants';
 
