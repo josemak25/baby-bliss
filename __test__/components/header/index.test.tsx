f25592b6ef0183c12878c9fbe5ebb83b50f12aff
josemak25 <amakirij@gmail.com>
2020-03-01 17:35:11 +0100

    add test for header component

---
+++ __test__/components/header/index.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { render } from '@testing-library/react-native';
+import Theme from '../../../src/theme';
+import Header from '../../../src/components/header';
+
+jest.mock('react-navigation-stack', () => ({
+  useHeaderHeight: jest.fn().mockReturnValue(60)
+}));
+
+describe('TEST HEADER COMPONENT(<Header/>)', () => {
+  const { getByTestId } = render(
+    <Theme>
+      <Header testID="header-container">
+        <View>
+          <Text>header content</Text>
+        </View>
+      </Header>
+    </Theme>
+  );
+
+  test('It renders the header <Header/> component accurately', () => {
+    const cardContainer = getByTestId('header-container');
+    expect(cardContainer).toBeTruthy();
+  });
+
+  test('It renders other components passed as children to the header <Header/> component accurately', () => {
+    const cardContainer = getByTestId('header-container');
+    expect(cardContainer.children).toBeTruthy();
+  });
+});
