013e03b9c25172f90ad80fffe1b97008691be694
Izuking <izuking@okra.ng>
2020-03-02 11:13:57 +0100

    ch-add-snackbar-for-action-feedback

---
+++ src/components/UI/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const OfflineContainer = styled.View`
+  background-color: #f42850;
+  height: 30px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  position: absolute;
+  top: 30px;
+`;
+
+export const OfflineText = styled.Text`
+  color: #fff;
+`;
