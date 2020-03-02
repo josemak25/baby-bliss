2feafdba1f5c12ef8b059ec1e7f6cdf79bee6d6e
Izuking <izuking@okra.ng>
2020-03-02 11:12:32 +0100

    rf-minor-refactor

--- __test__/config/jest.setup.ts
+++ __test__/config/jest.setup.ts
@@ -1,6 +1,14 @@
 import { cleanup } from '@testing-library/react-native';
 
 // Clean all mounted node elements after each test
+jest.mock('Linking', () => ({
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  openURL: jest.fn().mockReturnValue(Promise.resolve()),
+  canOpenURL: jest.fn().mockReturnValue(Promise.resolve()),
+  getInitialURL: jest.fn().mockReturnValue(Promise.resolve())
+}));
+
 afterEach(cleanup);
 
 // Clean all mounted node elements after all tests
--- __test__/utils/applyScale.test.ts
+++ __test__/utils/applyScale.test.ts
@@ -1,8 +1,6 @@
 import applyScale from '../../src/utils/applyScale';
 import Devices from '../../src/libs/responsiveImage/device';
 
-// jest.mock('Dimensions');
-
 describe('TEST IMAGE SCALING FUNCTIONALITY', () => {
   test('get device screen size and return scaled value', () => {
     const scaledScreenSize = applyScale(Devices.scale);
