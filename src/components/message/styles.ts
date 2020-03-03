02a3ff37730c9a95c81b8b493edd85124b9e1765
josemak25 <amakirij@gmail.com>
2020-03-03 13:38:25 +0100

    add post message component styles

---
+++ src/components/message/styles.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+`;
+
+export const MediaInsertContainer = styled.TouchableHighlight`
+  width: 25px;
+  height: 25px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.INACTIVE_ICON_COLOR};
+  border-radius: ${25 / 2}px;
+  margin-right: 5px;
+`;
+
+export const EmojiContainer = styled.TouchableHighlight`
+  width: 25px;
+  height: 25px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.BD_DARK_COLOR};
+  border-radius: ${25 / 2}px;
+  margin: 0 10px;
+  margin-left: 5px;
+`;
+
+export const SendContainer = styled.TouchableHighlight`
+  width: 25px;
+  height: 25px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.BD_DARK_COLOR};
+  border-radius: ${25 / 2}px;
+`;
+
+export const MessageInput = styled.TextInput`
+  flex: 1;
+  height: 100%;
+  padding: 0px 5px;
+`;
