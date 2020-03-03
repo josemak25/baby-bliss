5a51db04f35439be2a5143c270028742cb8bf29d
Izuking <izuking@okra.ng>
2020-03-03 10:42:20 +0100

    ft-add test for LOAD_POSTS action type

---
+++ __test__/api/index.test.ts
@@ -0,0 +1,21 @@
+import API from '../../src/lib/api';
+import post from '../../src/libs/dummyPost.json';
+
+describe('TEST POST ACTIONS', function() {
+  test('should return non empty array of posts for the case of LOAD_POSTS action type', async () => {
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        statusCode: 200,
+        message: 'success',
+        payload: [post]
+      })
+    );
+
+    const response = await API.get('/posts');
+    expect(response).toBeDefined();
+    expect(response).toBeTruthy();
+    expect(response.statusCode).toBe(200);
+    expect(response.message).toBe('success');
+    expect(response.payload).toEqual([post]);
+  });
+});
