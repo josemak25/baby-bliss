9b5573391d9836fc7ca122cea137bdea5e4f1047
josemak25 <amakirij@gmail.com>
2020-03-06 08:51:15 +0100

    add community screen component and implemented general and community route fuctionality

--- src/screens/community/index.tsx
+++ src/screens/community/index.tsx
@@ -1,30 +1,133 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+import { Animated, Easing, Dimensions, StatusBar } from 'react-native';
+import LottieView from 'lottie-react-native';
+import { TabView, TabBar } from 'react-native-tab-view';
+import { useStoreContext } from '../../store';
 
-import Button from '../../components/button';
-import { Container, Welcome } from './styles';
+import GeneralRouteContainer from './generalRoute';
+import RouteContainer from './routeContainer';
+import { useThemeContext } from '../../theme';
 import { NavigationInterface } from '../../constants';
+
+import { LogoContainer, Logo, AskQuestion } from './styles';
+import { CategoryInterface } from '../../store/category/types';
 
 interface CommunityScreenProp extends NavigationInterface {
   testID?: string;
 }
 
 export default function CommunityScreen(props: CommunityScreenProp) {
+  const { colors, fonts } = useThemeContext();
+
+  const [{ categoryState }] = useStoreContext();
+
+  useEffect(() => {
+    startLikeAnimation(null);
+  }, []);
+
+  const [state, setState] = useState({
+    animateMessageIcon: new Animated.Value(0),
+    currentRoute: 0,
+    communityRoutes: [
+      { key: 'general', title: 'general' },
+      ...categoryState.category
+    ]
+  });
+
+  const startLikeAnimation = (userPressed: string | null) => {
+    state.animateMessageIcon.setValue(0);
+    Animated.timing(state.animateMessageIcon, {
+      toValue: 1,
+      duration: 1300,
+      easing: Easing.linear,
+      useNativeDriver: true
+    }).start(() => (userPressed ? askQuestion() : null));
+  };
+
+  const askQuestion = () => {
+    props.navigation.navigate('PostQuestionScreen');
+  };
+
+  const renderTabBar = (props: any) => (
+    <TabBar
+      {...props}
+      indicatorStyle={{
+        backgroundColor: colors.POST_TIP_COLOR,
+        width: 60,
+        marginLeft: 20
+      }}
+      style={{ backgroundColor: colors.BD_DARK_COLOR, paddingLeft: 18 }}
+      activeColor={colors.FONT_DARK_COLOR}
+      inactiveColor={colors.FONT_LIGHT_COLOR}
+      labelStyle={{
+        fontFamily: fonts.MONTSERRAT_BOLD,
+        textTransform: 'capitalize',
+        width: 150
+      }}
+      tabStyle={{ height: 50, width: 150 }}
+      scrollEnabled={true}
+    />
+  );
+
+  const renderScene = ({ route }) => {
+    switch (route.key) {
+      case 'general':
+        return <GeneralRouteContainer navigation={props.navigation} />;
+      default:
+        return <RouteContainer navigation={props.navigation} />;
+    }
+  };
+
   return (
-    <Container testID="Community">
-      <Button title="Community Screen button" />
-      <Welcome>Community Screen</Welcome>
-    </Container>
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <AskQuestion
+        style={{
+          shadowColor: colors.INACTIVE_ICON_COLOR,
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.5,
+          shadowRadius: 5,
+          elevation: 4
+        }}
+        onPress={() => startLikeAnimation('userPressed')}
+      >
+        <LottieView
+          source={require('../../../assets/animations/ask_question.json')}
+          style={{
+            height: 60,
+            width: 60,
+            alignContent: 'center',
+            justifyContent: 'center',
+            top: 1,
+            right: 0.2
+          }}
+          progress={state.animateMessageIcon}
+        />
+      </AskQuestion>
+      <TabView
+        navigationState={{
+          index: state.currentRoute,
+          routes: state.communityRoutes
+        }}
+        renderScene={renderScene}
+        onIndexChange={currentRoute => setState({ ...state, currentRoute })}
+        initialLayout={{ width: Dimensions.get('window').width }}
+        renderTabBar={renderTabBar}
+        swipeVelocityImpact={10}
+        lazy={true}
+      />
+    </Fragment>
   );
 }
 
-CommunityScreen.navigationOptions = ({ navigationOptions, navigation }) => {
-  const { navigationBackButton } = navigation.state;
-
-  return {
-    ...navigationBackButton,
-    ...navigationOptions,
-    headerTitle: () => null,
-    headerLeft: () => null,
-    headerStyle: { backgroundColor: '#F4F8FB' }
-  };
-};
+CommunityScreen.navigationOptions = () => ({
+  headerTitle: () => null,
+  headerLeft: () => (
+    <LogoContainer>
+      <Logo
+        source={require('../../../assets/images/logo.png')}
+        style={{ resizeMode: 'contain' }}
+      />
+    </LogoContainer>
+  )
+});
