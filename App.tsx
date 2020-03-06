cc95d444fdd4f0c276a2ca1392bfa1f16c230091
josemak25 <amakirij@gmail.com>
2020-03-06 09:11:56 +0100

    add react native screen package for faster in app navigations

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
+import { enableScreens } from 'react-native-screens';
 import AppLoading from './src/components/AppLoading';
 import AppRouter from './src';
 import './src/config';
 
 export default function App() {
   const [isAppReady, setIsAppReady] = useState(false);
+
+  enableScreens();
 
   return isAppReady ? (
     <AppRouter />
