c35d9cc7380f3fcb964e3a1f00d9fdcba7cfa4d0
josemak25 <amakirij@gmail.com>
2020-03-06 08:34:25 +0100

    add fix to app card size

--- src/components/card/styles.ts
+++ src/components/card/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.TouchableOpacity`
   width: 100%;
-  min-height: 300px;
+  height: 300px;
   align-items: center;
   background-color: ${({ theme }) => theme.colors.BD_DARK_COLOR};
 `;
