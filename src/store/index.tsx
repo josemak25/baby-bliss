78b715b58e2c6cdf358084d16a6c34a8f44e7552
josemak25 <amakirij@gmail.com>
2020-03-06 08:19:15 +0100

    add change to store and implement user grid and post category states to combined reducer for global use on store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -9,13 +9,17 @@ import useCombinedReducers from '../utils/useCombinedReducers';
 
 import postsReducer, { postInitialState } from './posts/reducer';
 import userReducer, { userInitialState } from './user/reducer';
+import gridSizeReducer, { gridInitialState } from './grid/reducer';
+import postCategoryReducer, { categoryInitialState } from './category/reducer';
 
 const StoreContext = createContext(null);
 
 const StoreProvider: FunctionComponent = ({ children }) => {
   const [state, dispatch] = useCombinedReducers({
-    user: useReducer(userReducer, userInitialState),
-    posts: useReducer(postsReducer, postInitialState)
+    userState: useReducer(userReducer, userInitialState),
+    postState: useReducer(postsReducer, postInitialState),
+    grid: useReducer(gridSizeReducer, gridInitialState),
+    categoryState: useReducer(postCategoryReducer, categoryInitialState)
   });
 
   return (
