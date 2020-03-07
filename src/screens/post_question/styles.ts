5e07fc903bbc9cc276e077a675144811c6ea2d9b
josemak25 <amakirij@gmail.com>
2020-03-07 08:19:16 +0100

    add post questions component styles

--- src/screens/post_question/styles.ts
+++ src/screens/post_question/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import applyScale from '../../utils/applyScale';
 
 export const Container = styled.View`
   flex: 1;
@@ -23,14 +24,14 @@ export const HeaderTitle = styled.Text`
 export const QuestionContainer = styled.View`
   flex: 1;
   width: 100%;
-  margin-top: 30px;
+  margin-top: 20px;
   padding: 0px 20px;
-  border: 1px;
+  justify-content: space-evenly;
 `;
 
 export const PostTitle = styled.Text`
   font-size: ${({ theme }) => theme.fonts.LARGE_SIZE - 2}px;
-  font-family: ${({ theme }) => theme.fonts.IBM_SANS_REGULAR};
+  font-family: ${({ theme }) => theme.fonts.IBM_SANS_BOLD};
   color: ${({ theme }) => theme.colors.FONT_LIGHT_COLOR};
   text-transform: uppercase;
 `;
@@ -39,23 +40,69 @@ export const PostTitleInput = styled.TextInput`
   font-size: ${({ theme }) => theme.fonts.LARGE_SIZE}px;
   font-family: ${({ theme }) => theme.fonts.IBM_SANS_REGULAR};
   color: ${({ theme }) => theme.colors.FONT_LIGHT_COLOR};
-  padding-top: 10px;
-  padding-bottom: 10px;
+  padding: 5px 10px;
+  margin: 15px 0px;
+  background-color: ${({ theme }) => theme.colors.BG_LIGHT_COLOR};
+  border-radius: 3px;
 `;
 
-export const PostDescription = styled.Text`
+export const PostDescriptionTitle = styled.Text`
   font-size: ${({ theme }) => theme.fonts.LARGE_SIZE - 2}px;
-  font-family: ${({ theme }) => theme.fonts.IBM_SANS_REGULAR};
+  font-family: ${({ theme }) => theme.fonts.IBM_SANS_BOLD};
   color: ${({ theme }) => theme.colors.FONT_LIGHT_COLOR};
   text-transform: uppercase;
-  margin-top: 20px;
-  border: 1px;
+  margin-top: 15px;
 `;
 
 export const PostDescriptionInput = styled.TextInput`
   font-size: ${({ theme }) => theme.fonts.LARGE_SIZE}px;
   font-family: ${({ theme }) => theme.fonts.IBM_SANS_REGULAR};
   color: ${({ theme }) => theme.colors.FONT_LIGHT_COLOR};
-  padding-top: 10px;
-  padding-bottom: 10px;
+  padding: 5px 10px;
+  margin: 15px 0px;
+  background-color: ${({ theme }) => theme.colors.BG_LIGHT_COLOR};
+  border-radius: 3px;
+`;
+
+export const PostCategoryTitle = styled.Text`
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE - 2}px;
+  font-family: ${({ theme }) => theme.fonts.IBM_SANS_BOLD};
+  color: ${({ theme }) => theme.colors.FONT_LIGHT_COLOR};
+  text-transform: uppercase;
+  margin-top: 15px;
+`;
+
+export const PostCategoryContainer = styled.View`
+  height: 30px;
+  margin-top: 10px;
+`;
+
+export const SelectImageContainer = styled.TouchableOpacity`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: 40px;
+`;
+
+export const SelectImageTitle = styled.Text`
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE - 2}px;
+  font-family: ${({ theme }) => theme.fonts.IBM_SANS_REGULAR};
+  color: ${({ theme }) => theme.colors.FONT_LIGHT_COLOR};
+  text-transform: capitalize;
+  margin-top: 10px;
+`;
+
+export const PostButtonContainer = styled.View`
+  width: 100%;
+  height: 120px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SelectedImageContanier = styled.View`
+  width: 100%;
+  height: ${applyScale(200)}px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5px;
 `;
